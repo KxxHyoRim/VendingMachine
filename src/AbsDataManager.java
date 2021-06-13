c74cedb6f18e58b7e5b3db097391f32172fef54f
yeseul106 <cho898989@naver.com>
2021-06-13 09:14:29 +0900

    DataManager_yeseul

--- src/AbsDataManager.java
+++ src/AbsDataManager.java
@@ -1,15 +1,22 @@
 public class AbsDataManager {
 
     public int price;
-    public int needed_water;
     public int needed_sugar;
+    public int needed_creamer;
+    public int needed_coffeeBeans;
+    public int needed_yulmu;
+    public int needed_cocoa;
 
 
 
-    AbsDataManager(int p, int w, int s) {
-        price = p;
-        needed_water = w;
-        needed_sugar = s;
+
+    AbsDataManager(int price, int sugar, int beans, int creamer, int yulmu, int cocoa) {
+        this.price = price;
+        this.needed_sugar = sugar;
+        this.needed_creamer = creamer;
+        this.needed_coffeeBeans = beans;
+        this.needed_yulmu = yulmu;
+        this.needed_cocoa = cocoa;
     }
 
 
--- src/Controller.java
+++ src/Controller.java
@@ -1,14 +1,22 @@
 public class Controller {
 
-    int minMenuPrice;
+    int cash;
+    boolean bool_ledOn[];
+    DataManager dm;
 
     Controller() {
-        minMenuPrice =  new DataManager().setMinPrice();
-        System.out.println("minMenu :: " + minMenuPrice);
 
+        dm = new DataManager();
+
+        //LED state 초기화
+        bool_ledOn = new boolean[9];
+        for(int i=0; i<9; i++){
+            bool_ledOn[i] = false;
+        }
     }
 
+    void checkLEDon(int cash){
 
-
+    }
 }
 
