f33d05a9ca76568f375411ee62ce774acc0756e0
KxxHyoRim <rlagyfla0709@gmail.com>
2021-06-15 01:48:57 +0900

    UserPanel.java

--- src/UserPanel.java
+++ src/UserPanel.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
 
 public class UserPanel extends JFrame {
 
     private MyPanel panel = new MyPanel();
+    private DataManager dm = new DataManager();
+    HashMap<String, AbsDataManager> list_menu = dm.list_menu;
+
 
     public UserPanel(){
         setTitle("Vending Machine");
@@ -13,9 +17,11 @@ public class UserPanel extends JFrame {
         panel.setLayout(null);
         setContentPane(panel);
 
-        new oneMenu(152,210, "Sugar",600);
-        new oneMenu(215,210, "Milk",600);
-        new oneMenu(278,210, "Black",500);
+        System.out.println(list_menu.keySet());
+
+        new oneMenu(147,210, "Sugar", list_menu.get("Sugar").price);
+        new oneMenu(210,210, "Milk",list_menu.get("Milk").price);
+        new oneMenu(273,210, "Black",list_menu.get("Black").price);
 
         setAlwaysOnTop(true);
         setLocation(1300, 0);
@@ -29,23 +35,24 @@ public class UserPanel extends JFrame {
 
             JLabel name = new JLabel();
             name.setFont(new Font("Arial", Font.BOLD, 15));
-            name.setVerticalAlignment(SwingConstants.CENTER);
+            name.setHorizontalAlignment(SwingConstants.CENTER);
             name.setText(n);
+//            name.setBorder(BorderFactory.createLineBorder(Color.black));
             name.setBackground(Color.BLUE);
             name.setOpaque(false);
             name.setForeground(Color.BLACK);
-            name.setSize(100, 15);
+            name.setSize(50, 15);
             name.setLocation(locX, locY);
             panel.add(name);
 
             JLabel price = new JLabel();
             price.setFont(new Font("Arial", Font.PLAIN, 13));
-            price.setVerticalAlignment(SwingConstants.CENTER);
+            price.setHorizontalAlignment(SwingConstants.CENTER);
             price.setText(String.valueOf(p));
             price.setBackground(Color.gray);
             price.setForeground(Color.BLACK);
-            price.setSize(100, 15);
-            price.setLocation(locX + 11, locY + 25);
+            price.setSize(50, 15);
+            price.setLocation(locX, locY + 25);
             panel.add(price);
 
             JButton c1 = new JButton();
@@ -55,7 +62,7 @@ public class UserPanel extends JFrame {
             c1.setOpaque(false);
             c1.setForeground(Color.RED);
             c1.setSize(50, 15);
-            c1.setLocation(locX - 5, locY + 60);
+            c1.setLocation(locX, locY + 60);
             panel.add(c1);
         }
 
