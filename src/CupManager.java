58efb6f763b9fc5d22f24d2a4d56df9d56a68937
yeseul106 <cho898989@naver.com>
2021-06-15 09:04:47 +0900

    완성이었으면 좋겠는 완성본 (출력을 추가해봤어요.)

--- src/CupManager.java
+++ src/CupManager.java
@@ -18,11 +18,15 @@ public class CupManager {
     }
 
     static void decreaseCupNumber(){
+        System.out.println("현재 컵의 갯수는 " + curCupNumber + " 입니다.");
         curCupNumber--;
+        System.out.println("감소된 후, 컵의 갯수는 " + curCupNumber + " 입니다.");
     }
 
     static void increaseCupNumber(int addedNumber){
+        System.out.println("현재 컵의 갯수는 " + curCupNumber + " 입니다.");
         curCupNumber+=addedNumber;
+        System.out.println("추가된 후, 컵의 갯수는 " + curCupNumber + " 입니다.");
     }
 
     static void dropCup(){
--- src/WaterManager.java
+++ src/WaterManager.java
@@ -21,10 +21,14 @@ public class WaterManager {
     }
 
     static void decreaseWaterAmount(int needed_waterAmount){
+        System.out.println("현재 물의 양은 " + curWaterAmount + " 입니다.");
         curWaterAmount -= needed_waterAmount;
+        System.out.println("감소된 후, 물의 양은 " + curWaterAmount + " 입니다.");
     }
 
     static void increaseWaterAmount(int additionalAmount){
+        System.out.println("현재 물의 양은 " + curWaterAmount + " 입니다.");
         curWaterAmount += additionalAmount;
+        System.out.println("증가된 후, 물의 양은 " + curWaterAmount + " 입니다.");
     }
 }
